288ae181496efbba32eee1a51ab1c8fa808c21a6
Nicolò Martini <electroportal.net@gmail.com>
2012-12-28 18:07:38 +0100

    [Transformations] Common transformations implementation and tests

---
+++ src/TreeBuilder/Transformation/Common.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * This file is part of TreeBuilder.
+ *
+ * (c) 2012 Nicolò Martini
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace TreeBuilder\Transformation;
+
+/**
+ * A collection of common transformations
+ *
+ * @package    TreeBuilder
+ * @author     Nicolò Martini <nicmartnic@gmail.com>
+ */
+class Common
+{
+    /**
+     * Returns the element of an array
+     *
+     * @param array $array  The value to transform
+     * @param string $key   The name of the item key
+     * @return mixed
+     */
+    public static function element($array, $key)
+    {
+        return $array[$key];
+    }
+
+    /**
+     * @param $value
+     * @return mixed
+     */
+    public static function identity($value)
+    {
+        return $value;
+    }
+
+    /**
+     * @param $object
+     * @param string $property
+     * @return mixed
+     */
+    public static function property($object, $property)
+    {
+        return $object->$property;
+    }
+
+    /**
+     * Call a method of the object and returns its results
+     *
+     * @param $object
+     * @param string $methodName
+     * @return mixed
+     */
+    public static function method($object, $methodName)
+    {
+        $args = func_get_args();
+        $object = array_shift($args);
+        $methodName = array_shift($args);
+
+        return call_user_func_array(array($object, $methodName), $args);
+    }
+}
---
+++ tests/TreeBuilder/Transformation/CommonTest.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This file is part of TreeBuilder.
+ *
+ * (c) 2012 Nicolò Martini
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace TreeBuilder\Test\Transformation;
+
+/**
+ * Unit tests for class TransformationProviderTest
+ *
+ * @package    TreeBuilder
+ * @author     Nicolò Martini <nicmartnic@gmail.com>
+ */
+use TreeBuilder\Transformation\Common;
+
+class CommonTest extends \PHPUnit_Framework_TestCase
+{
+    public function testElement()
+    {
+        $common = new Common();
+
+        $array = array('key1' => 'value1', 'key2' => 123);
+
+        $this->assertEquals('value1', $common->element($array, 'key1'));
+        $this->assertEquals(123, $common->element($array, 'key2'));
+    }
+
+    public function testIdentity()
+    {
+        $common = new Common();
+
+        $this->assertEquals(12345, $common->identity(12345));
+    }
+
+    public function testProperty()
+    {
+        $common = new Common;
+
+        $this->assertEquals('ciao', $common->property(new Mock, 'prop'));
+    }
+
+    public function testMethod()
+    {
+        $common = new Common;
+
+        $this->assertEquals('bleahxxxyyy', $common->method(new Mock, 'getBlaBla', 'xxx', 'yyy'));
+    }
+}
+
+class Mock
+{
+    public $prop = 'ciao';
+
+    public function getBlaBla($var1, $var2) { return 'bleah' . $var1 . $var2; }
+}
