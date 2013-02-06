f320b637c3887eb92232dfa0dc924795baae7211
Nicolò Martini <electroportal.net@gmail.com>
2013-02-06 18:00:53 +0100

    PropertyAccessResolver: implementation

--- src/TreeBuilder/Transformation/PropertyAccessResolver.php
+++ src/TreeBuilder/Transformation/PropertyAccessResolver.php
@@ -1,56 +1,47 @@
 <?php
-/**
- * Clavis is an Integrated Library Management System developed by Comperio srl.
- * Discovery is an OPAC bundled witch Clavis.
- * Copyright (C) 2000 - 2012 Comperio srl
+/*
+ * This file is part of TreeBuilder.
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License version 3 as published by
- * the Free Software Foundation with the addition of the following permission
- * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
- * WORK IN WHICH THE COPYRIGHT IS OWNED BY COMPERIO, COMPERIO DISCLAIMS THE
- * WARRANTY OF NON INFRINGEMENT  OF THIRD PARTY RIGHTS.
+ * (c) 2013 Nicolò Martini
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program; if not, see http://www.gnu.org/licenses or write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301 USA.
- *
- * You can contact Comperio srl headquarters at Via Nazionale, 154
- * 35048 Stanghella (Padova), ITALY, or at email address info@comperio.it.
- * Website: http://www.comperio.it
- *
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU Affero General Public License version 3.
- *
- * In accordance with Section 7(b) of the GNU Affero General Public License
- * version 3, these Appropriate Legal Notices must retain the display of the
- * "Powered by Comperio" logo. If the display of the logo is not reasonably
- * feasible for technical reasons, the Appropriate Legal Notices must display
- * the words "Powered by Comperio".
- *
- * Sources should be available via "Download" link available in all pages.
- * All modifications and changes should be delivered via patches to base
- * source release to simplify identification of changes.
- *
- * For Commercial Licenses and support please contact Comperio.
- *
- * @author Nicolò Martini <nicolo@comperio.it>
- * @link http://www.comperio.it/
- * @copyright Copyright &copy; 2006-2012 Comperio srl
- * @license http://www.gnu.org/licenses/agpl.html GNU Affero General Public License
- * @version $Id$
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace TreeBuilder\Transformation;
 
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
-class PropertyAccessResolver {
+/**
+ * Class PropertyAccessResolver
+ */
+class PropertyAccessResolver implements ResolverInterface
+{
+    /**
+     * @var PropertyAccessorInterface
+     */
+    private $accessor;
+
+    /**
+     * @param PropertyAccessorInterface $accessor
+     */
+    public function __construct(PropertyAccessorInterface $accessor)
+    {
+        $this->accessor = $accessor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolve(array $args = array())
+    {
+        $accessor = $this->accessor;
+        $path = $args[0];
+
+        return function($item) use ($accessor, $path) {
+            return $accessor->getValue($item, $path);
+        };
+    }
+
 
 }
